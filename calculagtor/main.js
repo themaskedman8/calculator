e524acb1359ed597c9bb374b4cc0415ec717a0f3
themaskedman8 <138873592+themaskedman8@users.noreply.github.com>
2023-11-25 22:46:31 +0400

    Add files via upload

---
+++ calculagtor/main.js
@@ -0,0 +1,287 @@
+var input_1 = document.getElementById("input_1");
+var input_2 = document.getElementById("input_2");
+var input_3 = document.getElementById("input_3");
+var answer = document.getElementById("answer");
+
+function one() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 1");
+        input_3.value = 1;
+    }
+    else {
+        input_1.value = 1;
+        console.log("going to first, 1");
+    }
+    console.log("1");
+}
+
+function two() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 2");
+        input_3.value = 2;
+    }
+    else {
+        input_1.value = 2;
+        console.log("going to first, 2");
+    }
+    console.log(2);
+}
+
+function three() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 3");
+        input_3.value = 3;
+    }
+    else {
+        input_1.value = 3;
+        console.log("going to first, 3");
+    }
+    console.log("3");
+}
+
+function four() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 4");
+        input_3.value = 4;
+    }
+    else {
+        input_1.value = 4;
+        console.log("going to first, 4");
+    }
+    console.log("4");
+}
+
+function five() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 5");
+        input_3.value = 5;
+    }
+    else {
+        input_1.value = 5;
+        console.log("going to first, 5");
+    }
+    console.log("5");
+}
+
+function six() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 6");
+        input_3.value = 6;
+    }
+    else {
+        input_1.value = 6;
+        console.log("going to first, 6");
+    }
+    console.log("6");
+}
+
+function seven() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 7");
+        input_3.value = 7;
+    }
+    else {
+        input_1.value = 7;
+        console.log("going to first, 7");
+    }
+    console.log("7");
+}
+
+function eight() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 8");
+        input_3.value = 8;
+    }
+    else {
+        input_1.value = 8;
+        console.log("going to first, 8");
+    }
+    console.log("8");
+}
+
+function nine() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 9");
+        input_3.value = 9;
+    }
+    else {
+        input_1.value = 9;
+        console.log("going to first, 9");
+    }
+    console.log("9");
+}
+
+function zero() {
+    if (
+        input_1.value.trim() !== '' &&
+        (input_1.value == 1 ||
+            input_1.value == 2 ||
+            input_1.value == 3 ||
+            input_1.value == 4 ||
+            input_1.value == 5 ||
+            input_1.value == 6 ||
+            input_1.value == 7 ||
+            input_1.value == 8 ||
+            input_1.value == 9 ||
+            input_1.value == 0)
+    ) {
+        console.log("going to third, 0");
+        input_3.value = 0;
+    }
+    else {
+        input_1.value = 0;
+        console.log("going to first, 0");
+    }
+    console.log("0");
+}
+
+function submit() {
+    if (input_2.value == "+") {
+        var ans_add = Number(input_1.value) + Number(input_3.value);
+        document.getElementById("answer").value = Number(ans_add);
+        console.log("answer = " + ans_add);
+    }
+    if (input_2.value == "-") {
+        var ans_sub = Number(input_1.value) - Number(input_3.value);
+        document.getElementById("answer").value = Number(ans_sub);
+        console.log("answer = " + ans_sub);
+    }
+    if (input_2.value == "x") {
+        var ans_mul = Number(input_1.value) * Number(input_3.value);
+        document.getElementById("answer").value = Number(ans_mul);
+        console.log("answer = " + ans_mul);
+    }
+    if (input_2.value == "÷") {
+        var ans_div = Number(input_1.value) / Number(input_3.value);
+        document.getElementById("answer").value = Number(ans_div);
+        console.log("answer = " + ans_div);
+    }
+}
+
+function add() {
+    input_2.value = "+";
+    console.log("added");
+}
+
+function subtract() {
+    input_2.value = "-";
+    console.log("minused");
+}
+
+function multiply() {
+    input_2.value = "x";
+    console.log("multiplied");
+}
+
+function divide() {
+    input_2.value = "÷";
+    console.log("divided");
+}
